9aad46efb5abc918e7865245afd461e565b756fa
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-06-24 07:41:29 -0400

    Add demo of `calc` from TPIL.

--- Main.lean
+++ Main.lean
@@ -183,3 +183,17 @@ def α_to_m_β : Unit → IO UInt32 := (λ _ => pure 0)
 #check true
 #check false
 #check true <|> false
+
+variable (a b c d e : Nat)
+variable (h1 : a = b)
+variable (h2 : b = c + 1)
+variable (h3 : c = d)
+variable (h4 : e = 1 + d)
+
+theorem T : a = e :=
+  calc
+    a = b      := h1
+    _ = c + 1  := h2
+    _ = d + 1  := congrArg Nat.succ h3
+    _ = 1 + d  := Nat.add_comm d 1
+    _ = e      := Eq.symm h4
